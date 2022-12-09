0a2123528068ebd0222f37d2cea6176939e1560c
Bautista Pietraroia <bautistapietraroia@gmail.com>
2022-12-09 19:31:41 -0300

    recipes route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const axios = require('axios');
+const { Recipe, Diet } = require('../db');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,5 +10,45 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+/* Obteniendo todos los datos */
+
+const getApiInfo = async () => {
+  const apiUrl = await axios.get('https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5')
+  return apiUrl.data
+}
+const getDbInfo = async () => {
+  return await Recipe.findAll({
+    include: {
+      model: Diet,
+      attributes: ['name'],
+      through: {
+        attributes: [],
+      },
+    }
+  })
+}
+
+const getAllRecipes = async () => {
+  const apiInfo = await getApiInfo()
+  const dbInfo = await getDbInfo()
+  const result = apiInfo.results.concat(dbInfo)
+  return result
+}
+
+/* Routes */
+
+router.get('/recipes', async (req, res) => {
+  const { name } = req.query
+  let allRecipes = await getAllRecipes()
+
+  if (name) {
+    let recipeName = await allRecipes.filter(recipe => recipe.title.toLowerCase().includes(name.toLowerCase()))
+
+    if (recipeName.length) return res.send(recipeName)
+    return res.status(404).send('No se encontró ninguna receta con ese nombre')
+  }
+
+  return res.send(allRecipes)
+})
 
 module.exports = router;
